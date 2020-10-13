ab616006fba3882c5b2893f0f7a57436cebf2d17
wiethkaty <tomorrow.is.another.day1829@gmail.com>
2020-10-13 22:26:16 +0900

    Add: fmcocolo version

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ from google.cloud import bigquery
 PROJECT_ID = os.environ['GCP_PROJECT']
 BQ_DATASET = os.environ['DATASET']
 BQ_TABLE = os.environ['TABLE']
-SITE_URL = os.environ['SITE_URL']
+FM802_URL = os.environ['FM802_URL']
 BQ = bigquery.Client()
-
+FMCOCOLO_URL = os.environ['FMCOCOLO_URL']
+FMCOCOLO_TABLE = os.environ['FMCOCOLO_TABLE']
 
 def fm802_onair_info(request):
-    html = requests.get(SITE_URL)
+    html = requests.get(FM802_URL)
 
     soup = BeautifulSoup(html.content, "html.parser")
 
@@ -37,6 +38,34 @@ def fm802_onair_info(request):
     logging.info('Total record: ' + str(len(insert_list)))
 
     table_id = PROJECT_ID + '.' + BQ_DATASET + '.' + BQ_TABLE
+    table = BQ.get_table(table_id)
+    errors = BQ.insert_rows(table, insert_list)
+    if errors != []:
+        raise BigQueryError(errors)
+
+def fmcocoro_onair_info(request):
+    html = requests.get(FMCOCOLO_URL)
+
+    soup = BeautifulSoup(html.content, "html.parser")
+
+    noa_song_list = soup.find(class_="noa-song-list")
+    song_list = noa_song_list.find_all(["dt", "dd"])
+    insert_list = []
+    song_dict = {}
+    for song in song_list:
+        if song.name == "dt":
+            time = song.text + ":00"
+            song_dict["time"] = time
+        else:
+            song_dict["artist"] = song.find("span", class_="artist-name").text
+            song_dict["song"] = song.find("span", class_="song-name").text
+        if len(song_dict) == 3:
+            insert_list.append(song_dict.copy())
+            song_dict.clear()
+
+    logging.info('Total record: ' + str(len(insert_list)))
+
+    table_id = PROJECT_ID + '.' + BQ_DATASET + '.' + FMCOCOLO_TABLE
     table = BQ.get_table(table_id)
     errors = BQ.insert_rows(table, insert_list)
     if errors != []:
